f8b3906899c34ac9dc82604a21969026fd4cce31
Adam <adamdabrowski04@gmail.com>
2015-02-13 17:27:25 +0100

    bla blaasdasdasd

--- src/Main.java
+++ src/Main.java
@@ -44,8 +44,8 @@ class BoxWeight extends Box {
 class Main {
     public static void main(String args[]) {
         BoxWeight mybox1 = new BoxWeight(10, 20, 15, 34.3);
-        BoxWeight mybox2 = new BoxWeight(2, 3, 4, 0.076);
-        
+        BoxWeight mybox2 = new BoxWeight(3, 3, 4, 0.076);
+
         double vol;
         vol = mybox1.volume();
         System.out.println("Volume of mybox1 is " + vol);
